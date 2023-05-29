85ce6ad2bfe9b5307a6afd2e4dbdf7fc711eba3d
Nitin <132347798+nitin8857888@users.noreply.github.com>
2023-05-29 17:12:15 +0530

    completely working

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <script setup>
-import { ref } from "vue";
-const counter = ref(0);
-const increment = () => {
-  counter.value++;
-};
-const decrement = () => {
-  counter.value--;
-};
+import { defineComponent } from "vue";
+import Counter from "./components/Counter.vue";
+
+defineComponent({
+  components: { Counter },
+});
 </script>
 
 <template>
-  <header>
-    <h1>Counter app</h1>
-    <p>{{ counter }}</p>
-    <button @click="increment">Increment</button>
-    <button @click="decrement">Decrement</button>
-  </header>
-
-  <main></main>
+  <Counter></Counter>
 </template>
 
 <style scoped></style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Counter app</h1>
+  <p>{{ counter }}</p>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+</template>
+<script setup>
+import { ref } from "vue";
+const counter = ref(0);
+const increment = () => {
+  counter.value++;
+};
+const decrement = () => {
+  counter.value--;
+};
+</script>
+<style></style>
