e74b044a587c6a67d82b39d1ed9d866680943db4
Nitin <132347798+nitin8857888@users.noreply.github.com>
2023-05-29 15:57:21 +0530

    first

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+const counter = ref(0);
+const increment = () => {
+  counter.value++;
+};
+const decrement = () => {
+  counter.value--;
+};
+</script>
+
+<template>
+  <header>
+    <h1>hello nitin</h1>
+    <p>{{ counter }}</p>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+  </header>
+
+  <main></main>
+</template>
+
+<style scoped></style>
